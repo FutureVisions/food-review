70dc83e9eaaa74dd85a37b3ee4ec078d73647cff
Irvin Campos <irvincampos76@gmail.com>
2022-03-23 19:46:15 -0700

    added superuser for admin

--- main/settings.py
+++ main/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 from . import secret
-SECRET_KEY = secret.SECRETKEY
+SECRET_KEY = secret.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
